fae132eabd5b88067cb7c153a95059a65d0fb76d
Tom Serenander <tom@serenander.se>
2024-03-15 17:05:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Tenzies - a game with dice
+
+## Description
+Click the **Roll** button to get new numbers. Click the numbers you want to keep.
+
+Roll until you get all dice the same!
+
+When you accomplish this a great firework will appear on your screen!
+
+<br/>
+<img src="./tenzies.png" alt="Screenshot of Tenzies game." width="400px"/>
+
+## Technologies
+- HTML
+- CSS
+- JavaScript
+- React
+
+## Live link
+Tenzies is deployed here:
+[https://toms-tenzies.netlify.app](https://toms-tenzies.netlify.app)
+
+Please feel free to visit and try the game!
+
+## What I learned
+### Conditional styling
+
+### Conditionall rendering
+
+### Keep track of certain condition
---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from 'react'
+import Die from './Die'
+import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti'
+
+function App() {
+	const [dice, setDice] = useState(allNewDice())
+	const [tenzies, setTenzies] = useState(false)
+
+	useEffect(() => {
+		const firstValue = dice[0].value
+		const allSameValues = dice.every((die) => die.value === firstValue)
+		const allHeld = dice.every((die) => die.isHeld)
+		if (allSameValues && allHeld) {
+			setTenzies(true)
+		}
+	}, [dice])
+
+	function allNewDice() {
+		const diceArray = []
+		for (let i = 0; i < 10; i++) {
+			diceArray[i] = {
+				value: Math.ceil(6 * Math.random()),
+				isHeld: false,
+				id: nanoid(),
+			}
+		}
+		return diceArray
+	}
+
+	function handleClick(e) {
+		if (tenzies) {
+			setTenzies(false)
+			setDice(allNewDice)
+		} else {
+			setDice((prevDice) =>
+				prevDice.map((die) => ({
+					...die,
+					value: die.isHeld
+						? die.value
+						: Math.ceil(6 * Math.random()),
+				}))
+			)
+		}
+	}
+
+	function holdDice(id) {
+		setDice((prevDice) =>
+			prevDice.map((die) => ({
+				...die,
+				isHeld: die.id === id ? !die.isHeld : die.isHeld,
+			}))
+		)
+	}
+
+	const diceElements = dice.map((die) => (
+		<Die
+			key={die.id}
+			value={die.value}
+			isHeld={die.isHeld}
+			handleClick={() => holdDice(die.id)}
+		/>
+	))
+
+	return (
+		<main>
+			{tenzies && <Confetti />}
+			<h1 className="title">Tenzies</h1>
+			<p className="instructions">
+				Roll until all dice are the same. Click each die to freeze it at
+				its current value between rolls.
+			</p>
+			<div className="dice-container">{diceElements}</div>
+			<button className="roll-btn" id="roll-btn" onClick={handleClick}>
+				{tenzies ? 'New Game' : 'Roll'}
+			</button>
+		</main>
+	)
+}
+
+export default App
